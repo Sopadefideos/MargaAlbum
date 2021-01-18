869179b8abc9c25ffb095c6ed887a70161e895aa
Antonio Marfil Sánchez <48298879+Sopadefideos@users.noreply.github.com>
2021-01-18 20:29:15 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>Album</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body style="margin: 0px;">
+    <div style="display: flex; justify-content: center; align-items: center;">
+        <img src="sources\mapa.png" alt="" style="width: 65%; height: 65%; margin-left: auto; margin-right: auto;">
+    </div>
+    <div>
+        <a href="mapa.php" style="position: relative; bottom: 436px; left: 41.5%; color: red;">
+            <img src="sources\boton.png" alt="" style="width: 2.75%; height: 2.75%;">
+        </a>
+    </div>
+  
+</body>
+</html>
